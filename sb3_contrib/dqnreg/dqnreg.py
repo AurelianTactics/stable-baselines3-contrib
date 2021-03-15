f651a324dfe74932ed1a9a93715646125f1ac7c1
AurelianTactics <jamesdilorenzo@gmail.com>
2021-03-14 20:22:15 -0400

    Update dqnreg.py

--- sb3_contrib/dqnreg/dqnreg.py
+++ sb3_contrib/dqnreg/dqnreg.py
@@ -77,6 +77,8 @@ class DQNReg(DQN):
         # parameters for DQNReg
         self.dqnreg_loss_weight = dqnreg_loss_weight
 
+        print("testing obs space")
+        self.observation_space = policy.observation_space
         if _init_setup_model:
             self._setup_model()
 
